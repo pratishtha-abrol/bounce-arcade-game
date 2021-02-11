a4b9e5cd69a44c4d0bdb937601052a7592cd92bf
Pratishtha Abrol <pratishthaabrol@gmail.com>
2021-02-11 19:17:20 +0530

    board created

--- src/board.py
+++ src/board.py
@@ -13,6 +13,8 @@ class Map () :
         self.matrix = np.array([[" " for i in range(self.width)] for j in range(self.height)])
         self.create_sky()
         self.create_ground()
+        self.create_slider()
+        self.create_ball()
 
     def render(self):
         for y in range(3, self.height):
@@ -24,10 +26,17 @@ class Map () :
     def create_ground(self):
         y = self.height - 1
         for x in range(self.width):
-            self.matrix[y][x] = "T"
-
+            self.matrix[y][x] = "_"
 
     def create_sky(self): 
         for x in range(self.width):
             self.matrix[3][x] = "-"
 
+    def create_ball(self):
+        y = self.height - 6
+        self.matrix[y][2] = "o"
+
+    def create_slider(self):
+        y = self.height - 5
+        for x in range(5):
+            self.matrix[y][x] = "="
--- src/global_func.py
+++ src/global_func.py
@@ -5,7 +5,7 @@ import global_var
 from colorama import Fore, Back, Style
 
 def create_header():
-    print("Bounceee")
+    print("||  BOUNCE  ||    LIVES:    %s    |    BRICKS LEFT:    %s    |    SCORE:    %s" % (env.lives, global_var.BRICKS, global_var.SCORE))
 
 def print_board():
     create_header()
